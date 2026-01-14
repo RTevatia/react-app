abe0a0422de5a8c654f8e695a4917d53faa613af
Rahul Tevatia <rtevatia2@gmail.com>
2026-01-13 21:25:51 -0500

    feat(alert): support children content in Alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  return (<div>
+      <Alert>
+        Hello <span>World</span>
+      </Alert>
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  }
-
-  return (
-    <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
-    </div>
+  </div>
   );
 }
 
