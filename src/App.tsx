30b43028fa8b4cad3a85b52c3da71f8ed2e5497c
Rahul Tevatia <rtevatia2@gmail.com>
2026-01-15 07:13:45 -0500

    feat(ui): add dismissible alert with state control

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
   return (
     <div>
-      <Button onclick={() => console.log("Clicked")}>
-        My Button
-      </Button>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button onclick={() => setAlertVisibility(true)}>My Button</Button>
     </div>
   );
 }
